adf34e178a427207783e1857d7c4e987be6bdcb4
stobertonf <stobertonf@gmail.com>
2023-06-14 19:14:57 -0300

    feat(Home-Page): Creating the Home Module file.

--- lib/app/modules/home/home_module.dart
+++ lib/app/modules/home/home_module.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:chat_firebase_dio/app/modules/home/home_page.dart';
 import 'package:chat_firebase_dio/app/shared/constants/routes.dart';
+import 'package:chat_firebase_dio/app/modules/home/presenter/home_page.dart';
 
 class HomeModule extends Module {
   @override
