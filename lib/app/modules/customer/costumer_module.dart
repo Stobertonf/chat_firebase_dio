089b236b08095f3fe05594d12c8adbe965937d5d
stobertonf <stobertonf@gmail.com>
2023-06-15 03:20:16 -0300

    feat(Customer): Creating the Customer Module file.

---
+++ lib/app/modules/customer/costumer_module.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:chat_firebase_dio/app/shared/constants/routes.dart';
+import 'package:chat_firebase_dio/app/modules/home/presenter/home_page.dart';
+import 'package:chat_firebase_dio/app/modules/customer/presenter/customer_page.dart';
+
+class CustomerModule extends Module {
+  @override
+  List<ModularRoute> get routes => [
+        ChildRoute(
+          Modular.initialRoute,
+          child: (_, args) => const CustomerPage(),
+        ),
+        ChildRoute(
+          Routes.customer,
+          child: (_, args) => const CustomerPage(),
+        ),
+      ];
+}
