e087279c9b72760209c4be252cf586676cf92db1
stobertonf <stobertonf@gmail.com>
2023-06-15 03:19:51 -0300

    feat(Customer): Creating new route.

--- lib/app/modules/home/home_module.dart
+++ lib/app/modules/home/home_module.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:chat_firebase_dio/app/shared/constants/routes.dart';
 import 'package:chat_firebase_dio/app/modules/home/presenter/home_page.dart';
+import 'package:chat_firebase_dio/app/modules/customer/presenter/customer_page.dart';
 
 class HomeModule extends Module {
   @override
@@ -8,6 +9,10 @@ class HomeModule extends Module {
         ChildRoute(
           Modular.initialRoute,
           child: (_, args) => const HomePage(),
+        ),
+        ChildRoute(
+          Routes.customer,
+          child: (_, args) => const CustomerPage(),
         ),
       ];
 }
