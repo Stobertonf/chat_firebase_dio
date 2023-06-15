bdf5d6b204ca5d85f6bfee46b377b7634f981ded
stobertonf <stobertonf@gmail.com>
2023-06-15 05:02:55 -0300

    feat(Login-Page): Creating new route.

--- lib/app/shared/constants/routes.dart
+++ lib/app/shared/constants/routes.dart
@@ -2,6 +2,7 @@ class Routes {
   static const String start = '/';
   static const String chat = '/chat';
   static const String home = '/home';
+  static const String login = '/login';
   static const String splash = '/splash';
   static const String customer = '/customer';
 }
